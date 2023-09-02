c972b99aaedc5ff47d645831fc49ff65efef7111
Muguntha96 <muguntharamkumar1996@gmail.com>
2023-09-01 21:32:07 -0300

    add css styles

--- js/app.js
+++ js/app.js
@@ -1,49 +1,69 @@
 /*-------------------------------- Constants --------------------------------*/
 const difficultylevel = {
-  easy:8,
-  medium:12,
-  hard:16
+  easy: 8,
+  medium: 12,
+  hard: 16
 
 }
 console.log(difficultylevel)
 
 /*-------------------------------- Variables --------------------------------*/
 
-let firstIndex,card,count,level
+let firstIndex, card, count, level
 
 
 
 /*------------------------ Cached Element References ------------------------*/
-const buttons=document.querySelector("#buttons")
-const easyBtn=document.querySelector("#easy")
-const mediumBtn=document.querySelector("#medium")
-const hardBtn=document.querySelector("#hard")
+const buttons = document.querySelector("#buttons")
+const easyBtn = document.querySelector("#easy")
+const mediumBtn = document.querySelector("#medium")
+const hardBtn = document.querySelector("#hard")
+const imageContainer = document.querySelector("#image-container")
+
 //console.log(easyBtn)
 
 /*----------------------------- Event Listeners -----------------------------*/
 
-easyBtn.addEventListener('click',difficultyLevelButtonClick)
-mediumBtn.addEventListener('click',difficultyLevelButtonClick)
-hardBtn.addEventListener('click',difficultyLevelButtonClick)
+easyBtn.addEventListener('click', difficultyLevelButtonClick)
+mediumBtn.addEventListener('click', difficultyLevelButtonClick)
+hardBtn.addEventListener('click', difficultyLevelButtonClick)
 
 /*-------------------------------- Functions --------------------------------*/
 init()
-function init (){
-  firstIndex=0
-  card=[]
-  count=0
-  level=''
-      
+function init() {
+  firstIndex = 0
+  images = []
+  count = 0
+  level = ''
+
 }
 
 function difficultyLevelButtonClick(evt) {
-  if (evt.target === easyBtn) {
-    buttons.remove()
-  } else if (evt.target === mediumBtn) {
-    buttons.remove()
+  buttons.style.display = 'none'
+  level = evt.target.id
+  console.log(difficultylevel[level])
+  imageContainer.style.display='flex'
+  for (let i = 0; i < difficultylevel[level]/4; i++) {
+        const divSet = document.createElement('div')
+        divSet.className =`set${i}`
+          imageContainer.appendChild(divSet)
+          for(let i=0;i<4;i++){
+            const divImage=document.createElement('div')
+            divImage.className='card-images'
+           // divImage.id=`${i}`
+            
+           
+            
+            divSet.appendChild(divImage)
+            
+           
+          }
+     
+          
+      }
+     
+      console.log(imageContainer)
 
-  } else if (evt.target === hardBtn) {
-    buttons.remove()
-  }
-
+  
 }
+

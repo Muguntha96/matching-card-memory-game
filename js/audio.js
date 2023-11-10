069d342bbd3aad9f5a4bf5bec0848907227c123f
Muguntha96 <muguntharamkumar1996@gmail.com>
2023-11-10 12:22:11 -0400

    change in css

--- js/audio.js
+++ js/audio.js
@@ -1,25 +1,25 @@
 
-let correctMatch=new Audio('../assets/audio/matching-correct.mp3')
-let timeOut=new Audio('../assets/audio/time-out.mp3')
-let winMusic=new Audio('../assets/audio/winner-music.wav')
+let correctMatch = new Audio('../assets/audio/matching-correct.mp3')
+let timeOut = new Audio('../assets/audio/time-out.mp3')
+let winMusic = new Audio('../assets/audio/winner-music.wav')
 
-function validSelect(){
-  correctMatch.volume=0.25
+function validSelect() {
+  correctMatch.volume = 0.25
   correctMatch.play()
 
 }
- function timeCompleted(){
-  timeOut.volume=0.25
+function timeCompleted() {
+  timeOut.volume = 0.25
   timeOut.play()
 
- }
- function winnerMusic(){
-  winMusic.volume=0.25
+}
+function winnerMusic() {
+  winMusic.volume = 0.25
   winMusic.play()
 
- }
- export{
+}
+export {
   validSelect,
   timeCompleted,
   winnerMusic
- }
+}
